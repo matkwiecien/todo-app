2974e4547d5f4008b553d531d2785a22999e3308
Mateusz Kwiecien <m.kwiecien916@gmail.com>
2021-03-13 11:27:07 +0100

    Add saving task

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
+test('should create task when user click save and clear task input', () => {
+  //GIVEN
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const taskInput = screen.getByRole('textbox', {
+    name: /task:/i
+  })
+
+  //WHEN
+  userEvent.type(taskInput, "new task");
+  userEvent.click(screen.getByRole("button", { name: 'Save'}));
+
+  //THEN
+  expect(screen.getByText("new task")).toBeInTheDocument();
+  expect(taskInput).toHaveValue("")
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return (
-      <div>
+    const [ tasks, setTasks ] = useState<string[]>([]);
 
-      </div>
-  );
+    const [ task, setTask ] = useState<string>("");
+
+    const saveTask = () => {
+        setTasks(tasks => [task, ...tasks])
+        setTask('')
+    }
+
+    return (
+        <div>
+            <main>
+                <h1>TODO APP</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <form onSubmit={(event) => {
+                    event.preventDefault();
+                    saveTask();
+                }}>
+                    <label>
+                        Task:
+                        <input value={task} type="text" placeholder="Buy milk" name="todo" onChange={event => setTask(event.target.value)}/>
+                    </label>
+                    <button>Save</button>
+                </form>
+                <ul>
+                    {
+                        tasks.map((task, index) => <li key={index}>{task}</li>)
+                    }
+                </ul>
+            </main>
+        </div>
+    );
 }
 
 export default App;
