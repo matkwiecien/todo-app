bc25dbc56196378bd28714598e303e924f85184d
Mateusz Kwiecien <m.kwiecien916@gmail.com>
2023-02-28 08:32:11 +0100

    Add linter

--- src/Todos.test.tsx
+++ src/Todos.test.tsx
@@ -1,37 +1,36 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import Todos from './Todos';
+import Todos from './Todos'
 
 test('should create task when user click save and clear task input', () => {
-    //GIVEN
-    render(<Todos />);
-    const taskInput = screen.getByRole('textbox', {
-        name: /task:/i
-    })
+  // GIVEN
+  render(<Todos />)
+  const taskInput = screen.getByRole('textbox', {
+    name: /task:/i,
+  })
 
-    //WHEN
-    userEvent.type(taskInput, "new task");
-    userEvent.click(screen.getByRole("button", { name: 'Save'}));
+  // WHEN
+  userEvent.type(taskInput, 'new task')
+  userEvent.click(screen.getByRole('button', { name: 'Save' }))
 
-    //THEN
-    expect(screen.getByText("new task")).toBeInTheDocument();
-    expect(taskInput).toHaveValue("")
-});
+  // THEN
+  expect(screen.getByText('new task')).toBeInTheDocument()
+  expect(taskInput).toHaveValue('')
+})
 
 test('should remove task when user click delete button', () => {
-    //GIVEN
-    render(<Todos />);
-    const taskInput = screen.getByRole('textbox', {
-        name: /task:/i
-    })
+  // GIVEN
+  render(<Todos />)
+  const taskInput = screen.getByRole('textbox', {
+    name: /task:/i,
+  })
 
-    //WHEN
-    userEvent.type(taskInput, "new task");
-    userEvent.click(screen.getByRole("button", { name: 'Save'}));
-    userEvent.click(screen.getByRole("button", { name: 'Delete'}));
+  // WHEN
+  userEvent.type(taskInput, 'new task')
+  userEvent.click(screen.getByRole('button', { name: 'Save' }))
+  userEvent.click(screen.getByRole('button', { name: 'Delete' }))
 
-    //THEN
-    expect(screen.queryByText("new task")).not.toBeInTheDocument();
-
-});
+  // THEN
+  expect(screen.queryByText('new task')).not.toBeInTheDocument()
+})
--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,50 +1,58 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const Todos = () => {
-    const [ tasks, setTasks ] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<string[]>([])
 
-    const [ task, setTask ] = useState<string>("");
+  const [task, setTask] = useState<string>('')
 
-    const saveTask = () => {
-        setTasks(tasks => [task, ...tasks])
-        setTask('')
-    }
+  const saveTask = () => {
+    setTasks((tasks) => [task, ...tasks])
+    setTask('')
+  }
 
-    const deleteTask = (index: number) => {
-        setTasks(tasks => {
-            const tasksCopy = [...tasks];
-            tasksCopy.splice(index, 1);
-            return tasksCopy
-        })
-    }
-    return (
-        <>
-            <form onSubmit={(event) => {
-                event.preventDefault();
-                saveTask();
-            }}>
-                <label>
-                    Task:
-                    <input value={task} type="text" placeholder="Buy milk" name="todo" onChange={event => setTask(event.target.value)}/>
-                </label>
-                <button>Save</button>
-            </form>
-            <ul>
-                {
-                    tasks.map((task, index) => (
-                        <li key={index}>
-                            {task}
-                            <button onClick={() => {
-                                deleteTask(index)
-                            }}>
-                                Delete
-                            </button>
-                        </li>
-                    ))
-                }
-            </ul>
-        </>
-    )
+  const deleteTask = (index: number) => {
+    setTasks((tasks) => {
+      const tasksCopy = [...tasks]
+      tasksCopy.splice(index, 1)
+      return tasksCopy
+    })
+  }
+  return (
+    <>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
+          saveTask()
+        }}
+      >
+        <label>
+          Task:
+          <input
+            value={task}
+            type='text'
+            placeholder='Buy milk'
+            name='todo'
+            onChange={(event) => setTask(event.target.value)}
+          />
+        </label>
+        <button>Save</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button
+              onClick={() => {
+                deleteTask(index)
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
-export default Todos;
+export default Todos
